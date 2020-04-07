1fa11e1ae81e837c34fc57013386d6c0a1da7344
Scott Havens <scott.havens@ars.usda.gov>
2020-04-07 16:15:31 -0600

    getting the test setup

---
+++ tests/test_hor1d.py
@@ -0,0 +1,4 @@
+# Test the hor1d functions
+# Create gold files from IPW that have the hor1d results for all
+# 16 direction, then I can test if there is stuff actually
+# different between the directions
