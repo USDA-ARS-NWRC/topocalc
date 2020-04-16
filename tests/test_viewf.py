f20712504057fd2bcc2d2726b14044107c2e8706
Scott Havens <scott.havens@ars.usda.gov>
2020-04-16 13:10:28 -0600

    removed the ipw viewf test and added a fake dem to test the edge

--- tests/test_viewf.py
+++ tests/test_viewf.py
@@ -14,9 +14,22 @@ class TestViewf(unittest.TestCase):
     """Tests for `viewf` package."""
 
     def test_theory_edge(self):
-        """Test with theortical edge dem"""
+        """Test with infinite edge dem"""
 
         dem = np.ones((50, 50))
-        dem[:, :25] = 10000
+        dem[:, :25] = 100000
 
         svf, tvf = viewf(dem, spacing=10)
+
+        # The top should all be ones with 100% sky view
+        self.assertTrue(
+            np.array_equal(svf[:, :24],
+                           np.ones_like(svf[:, :24]))
+        )
+
+        # The edge should be 50% or 0.5 svf
+        np.testing.assert_allclose(
+            svf[:, 25],
+            0.5 * np.ones_like(svf[:, 25]),
+            atol=1e-3
+        )
