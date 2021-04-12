85e98eda3b03e8ce7854c6de086dc5fd907a882f
Scott Havens <scott.havens@ars.usda.gov>
2021-04-12 14:47:58 -0600

    relaxed osx viewf test

--- topocalc/tests/test_viewf.py
+++ topocalc/tests/test_viewf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-# from sys import platform
+from sys import platform
 
 import numpy as np
 
@@ -17,37 +17,37 @@ class TestViewf(unittest.TestCase):
         dem = np.ones((50, 50))
         dem[:, :25] = 100000
 
-        svf, tvf = viewf(dem, spacing=10, nangles=72)
+        svf, tvf = viewf(dem, spacing=10, nangles=360)
 
         # The top should all be ones with 100% sky view
         # OSX seems to have some difficulty with the edge
         # where linux does not. It is close to 1 but not quite
-        # if platform == 'darwin':
-        #     np.testing.assert_allclose(
-        #         svf[:, :24],
-        #         np.ones_like(svf[:, :24]),
-        #         atol=1e-2
-        #     )
+        if platform == 'darwin':
+            np.testing.assert_allclose(
+                svf[:, :24],
+                np.ones_like(svf[:, :24]),
+                atol=1e-2
+            )
 
-        #     # The edge should be 50% or 0.5 svf
-        #     np.testing.assert_allclose(
-        #         svf[:, 25],
-        #         0.5 * np.ones_like(svf[:, 25]),
-        #         atol=1e-2
-        #     )
+            # The edge should be 50% or 0.5 svf
+            np.testing.assert_allclose(
+                svf[:, 25],
+                0.5 * np.ones_like(svf[:, 25]),
+                atol=1e-2
+            )
 
-        # else:
-        np.testing.assert_array_equal(
-            svf[:, :24],
-            np.ones_like(svf[:, :24])
-        )
+        else:
+            np.testing.assert_array_equal(
+                svf[:, :24],
+                np.ones_like(svf[:, :24])
+            )
 
-        # The edge should be 50% or 0.5 svf
-        np.testing.assert_allclose(
-            svf[:, 25],
-            0.5 * np.ones_like(svf[:, 25]),
-            atol=1e-3
-        )
+            # The edge should be 50% or 0.5 svf
+            np.testing.assert_allclose(
+                svf[:, 25],
+                0.5 * np.ones_like(svf[:, 25]),
+                atol=1e-3
+            )
 
     def test_viewf_errors_dem(self):
         """Test viewf dem errors"""
